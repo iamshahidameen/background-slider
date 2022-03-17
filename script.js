0d499386bd7c18f9a5941782b69cbce523490319
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-18 02:21:07 +0500

    Added some JavaScripts to make background images dynamic based on the index number of active slide

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("background, World!");
+const body = document.body;
+const totalSlides = document.querySelectorAll('.active');
+const leftSlide = document.getElementById('left');
+const rightSlide = document.getElementById('right');
+
+let activeSlide = 2;
+
+setfirstSlidetoBg(activeSlide);
+
+function setfirstSlidetoBg(slideNumber){
+    body.style.backgroundImage = totalSlides[slideNumber].style.backgroundImage;
+}
